aff01fd1e6a2601c09a019fd6ccf470386fcdeba
Subhiksha-G <subhikshavkl@gmail.com>
2025-12-14 18:17:27 +0530

    1st version of holy grain website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="five-sections">
+        <header>Header</header>
+        <div className="three-sections">
+          <nav>Navigation</nav>
+          <main>Main</main>
+          <aside>Sidebar</aside>
+        </div>
+        <footer>Footer</footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
